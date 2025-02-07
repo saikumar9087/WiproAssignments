4b40a85122497bfa189722c1df89cb47e94bd887
saikumar9087 <109573123+saikumar9087@users.noreply.github.com>
2025-02-07 09:39:48 +0530

    Add files via upload

---
+++ WiproSample5/WiproSample5/IBank.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiproSample5
+{
+    internal interface IBank
+    {
+
+        public void BankManager();
+
+        public void BankEmployee();
+    }
+
+    internal interface IVault
+    {
+        public void VaultStock();
+        
+    }
+
+
+    public class BankM : IBank, IVault
+    {
+        public string ManagerName { get; set; }
+        public string ManagerId { get; set; }
+
+
+       
+
+
+
+        public void BankManager()
+        {
+            Console.WriteLine($"Manager name: {ManagerName} and id = {ManagerId}");
+            
+        }
+
+        public void VaultStock()
+        {
+            Console.WriteLine("Check the stock in the vault");
+
+        }
+
+
+
+        public void BankEmployee()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class BankE : IBank
+    {
+        public string EmpName { get; set; }
+        public string EmpId { get; set; }
+
+        
+
+       
+
+        public void BankEmployee()
+        {
+            Console.WriteLine($"Employee name: {EmpName} and id = {EmpId}");
+
+        }
+
+        public void BankManager()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WiproSample5/WiproSample5/Program.cs
@@ -0,0 +1,38 @@
+﻿using WiproSample5;
+
+internal class Program
+{
+    static class MathHelper //efficient for utility functions - static members & static functions
+    {
+        public static int count = 0;//static field
+        public static double square(double num)
+        {
+            return num * num;
+        }
+        public static double square(int num)
+        {
+            return num * num;
+        }
+        public static double Cube(double num)
+        {
+            return num * num * num;
+        }
+        public static void Dsiplay()
+        {
+            count++;
+            Console.WriteLine(count);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        BankM bm = new BankM() { ManagerId="101",ManagerName="sai"};
+
+        BankE be = new BankE() { EmpName = "sathwik", EmpId = "102" };
+
+        bm.BankManager();
+        bm.VaultStock();
+        be.BankEmployee();
+
+        MathHelper.Dsiplay();
+    }
+}
