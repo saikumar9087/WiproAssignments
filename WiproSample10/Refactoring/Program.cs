0b1faa23af7843ff2573e8d22feae0a9648eccbf
saikumar9087 <sai90878114@gmail.com>
2025-02-28 22:22:03 +0530

    SOLID principles and refactoring

---
+++ WiproSample10/Refactoring/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using RefactoringIssues.Models;
+using RefactoringIssues.Services;
+
+Console.WriteLine("Hello, World!");
+
+Employee per = new PermanentEmployee("Priya");
+Employee temp = new TemporaryEmployee("Shree");
+Employee freelancer = new Freelancer("Sid");
+Employee intern = new Intern("Navya");
+
+PayrollProcessor payroll = new PayrollProcessor();
+
+payroll.ProcessPayroll(per, 40);
+payroll.ProcessPayroll(freelancer, 12);
+payroll.ProcessPayroll(intern, 0);
+
